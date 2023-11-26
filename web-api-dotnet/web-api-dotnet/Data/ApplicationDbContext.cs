9b5e4350395cf4e060f9b39ece80a4bad9cce092
zerocool <williesuggsdev@gmail.com>
2023-11-26 10:27:38 -0600

    adding data folder

---
+++ web-api-dotnet/web-api-dotnet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_dotnet.Models;
+
+namespace web_api_dotnet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
