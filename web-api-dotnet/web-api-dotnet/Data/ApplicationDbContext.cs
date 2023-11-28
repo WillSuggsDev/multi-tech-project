82afa947997842d1491b0ee94605d61c2ce41928
zerocool <williesuggsdev@gmail.com>
2023-11-28 07:01:45 -0600

    added yml file to spin up all of my services

--- web-api-dotnet/web-api-dotnet/Data/ApplicationDbContext.cs
+++ web-api-dotnet/web-api-dotnet/Data/ApplicationDbContext.cs
@@ -9,5 +9,21 @@ namespace web_api_dotnet.Data
             : base(options) { }
 
         public DbSet<BlogPost> BlogPosts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            SeedData(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BlogPost>().HasData(
+                new BlogPost { Id = 1, Title = "Introduction to Docker", Content = "Docker is a powerful tool for containerization..." },
+                new BlogPost { Id = 2, Title = "Getting Started with React", Content = "React is a JavaScript library for building user interfaces..." }
+            );
+        }
     }
 }
